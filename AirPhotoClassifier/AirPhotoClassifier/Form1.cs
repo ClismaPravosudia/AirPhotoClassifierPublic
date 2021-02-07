a2e7fd7004ce508c563813197d387a14adbf9735
Name <e>
2021-02-07 23:24:16 +0300

    Вывели на экран входящую картинку и кашу после сегментации

--- AirPhotoClassifier/AirPhotoClassifier/Form1.cs
+++ AirPhotoClassifier/AirPhotoClassifier/Form1.cs
@@ -1,10 +1,14 @@
 ﻿using Emgu.CV;
+
+using Emgu.CV.Structure;
 using Emgu.CV.XImgproc;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,15 +22,18 @@ namespace AirPhotoClassifier
         {
             InitializeComponent();
 
-            Mat img = new Mat();
-            img = CvInvoke.Imread("D:\\WORKSPACE\\AirPhotoClassifierPublic\\AirPhotoClassifier\\AirPhotoClassifier\\Photo\\D1PGFIFSLX5R1521056003282.png", Emgu.CV.CvEnum.ImreadModes.AnyColor);
-            IInputArray image = img;
+            Mat image = CvInvoke.Imread("D:\\WORKSPACE\\AirPhotoClassifierPublic\\AirPhotoClassifier\\AirPhotoClassifier\\Photo\\D1PGFIFSLX5R1521056003282.png", Emgu.CV.CvEnum.ImreadModes.AnyColor);
 
-            int regionSize = 0;
+            imageBox1.Image = image;
+            
+            SupperpixelSLIC supperpixel = new SupperpixelSLIC(image, 
+                                                              SupperpixelSLIC.Algorithm.SLIC, 
+                                                              30, 
+                                                              1);
 
-            float ruler = 0;
+            Mat imageSLIC = new Mat(new Size(image.Width,image.Height), image.Depth,image.NumberOfChannels, supperpixel.Ptr, image.Step);
 
-            SupperpixelSLIC constructor = new SupperpixelSLIC(image, SupperpixelSLIC.Algorithm.SLIC, regionSize, ruler);
+            imageBox2.Image = imageSLIC.ToImage<Rgb,byte>();
         }
     }
 }
